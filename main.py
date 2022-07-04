3e1470043130722a29a4c316e4378927df7893d6
SarunasJ-gif <sarunasj82@gmail.com>
2022-07-05 00:00:50 +0300

    added random password generator

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [random.choice(letters) for _ in range(random.randint(6, 8))]
+    password_list += [random.choice(numbers) for _ in range(random.randint(2, 4))]
+    password_list += [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_info():
@@ -30,16 +48,13 @@ window = Tk()
 window.title("Password Generator")
 window.config(padx=20, pady=20)
 
-
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 lock_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_image)
 canvas.grid(column=1, row=0)
 
-
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
@@ -53,16 +68,15 @@ website_entry.focus()
 
 email_entry = Entry(width=39)
 email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "sarunasj82@gmail.com")
+email_entry.insert(0, "sarunas@gmail.com")
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=21, )
 password_entry.grid(column=1, row=3)
 
-generate_password = Button(text="Generate Password", width=14)
-generate_password.grid(column=2, row=3)
+password_button = Button(text="Generate Password", command=password_generator, width=14)
+password_button.grid(column=2, row=3)
 
-add_password = Button(text="Add",  command=save_info, width=33)
+add_password = Button(text="Add", command=save_info, width=33)
 add_password.grid(column=1, row=4, columnspan=2)
-
 
 window.mainloop()
